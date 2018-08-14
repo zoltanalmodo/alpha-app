22b112991cad7b7f9ff6d80c4e54545f5452d499
Constantin Kalinin <kostiantynkalinin@gmail.com>
2018-08-14 17:12:36 +0100

    Render data from Floorplandata object in Details component (#37)
    * test
    
    * Create Floor plan component #11
    
    * Integreate floor plan image into Floor plan component #25
    
    * Introduce state to Floor plan component #31
    
    * Create Details Component; Render Details component on click
    
    * Create collection of room objects; Render details component of specific room #32 #33
    
    * Render data from Floorplandata object in Details component
    
    * Render unique room information in Details component #35

--- src/FloorPlan/mapArea.js
+++ src/FloorPlan/mapArea.js
@@ -2,23 +2,23 @@ export default function mapArea() {
   return [
     {
       coords: "23,103,129,137",
-      id: "58"
+      id: 58
     },
     {
       coords: "23,137,130,169",
-      id: "59"
+      id: 59
     },
     {
       coords: "23,169,130,207",
-      id: "60"
+      id: 60
     },
     {
       coords: "23,207,106,235",
-      id: "61"
+      id: 61
     },
     {
       coords: "23,235,106,275",
-      id: "62"
+      id: 62
     }
   ]
 }
