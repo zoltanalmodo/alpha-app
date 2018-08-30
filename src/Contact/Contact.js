f75f68324d3f1428c2e9c86450dee3fe3bfd6ed4
Albert Pai <verdant.light@gmail.com>
2018-08-30 13:24:48 +0100

    Add content to Contact.js and FAQ.js (#57)
    * Add instructions to README.md
    
    * Correct typo
    
    * Update Reade file
    
    * Add hot loading
    
    * Change logo file
    
    * Add the cityView picture and rearrange the images folder
    
    * Update the cityView file
    
    * Apply Google font
    
    * Add Flexbox
    
    * Add content and div to App.js
    
    * Delete an old cityView image
    
    * Add Google Map API (google-maps-react)
    
    * Make flex-wrap work and minor change in style
    
    * Modify sentences
    
    * Use flex-basis. Improve small screen experience. Fix logo animation.
    
    * Rephrase a sentence.
    
    * Clean comments and move code to GoogleMap.css
    
    * No change to gitignore
    
    * Merge
    
    * Move AboutUs / container1 into a component
    
    * Update the introduction in AboutUs.js
    
    * Move Google Map files to a folder under AboutUs.
    
    *  Fix a class name in AbourUs.css.
    
    * Move Header to a new component
    
    * Move code in App.less to component less files
    
    * Rename classes in AboutUs.js and link to less file
    
    * Rename as AboutUs.less, rename classes and make it responsive
    
    * Mode div into FloorPlan.js, add a class and link to FloorPlan.less
    
    * Create FloorPlan.less and rotate floorplan on less-than-900px screens
    
    * Rename a class in Header.js and link to less file
    
    * Rename as Header.less, reanme a class, and make h2 responsive
    
    * Create Contact.js component and Contact.less
    
    * Create FAQ.js component and FAQ.less
    
    * Add room images
    
    * Update Contact.js
    
    updated contact information from external websites
    manyhandslondon.co.uk
    shapescollective.com
    
    * Resolve a conflict in Contact.js
    
    * Create a contact form (Contact.js) that renders all submission to Gmail
    
    * Typo
    
    * Move contact info to FAQ.js, create questions and improve responsiveness
    
    * Merge Contact.js and move contact details from FAQ to Contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import './Contact.less';
+import * as typeformEmbed from '@typeform/embed';
 
 export default class Contact extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.element = null;
+    }
+
+    componentDidMount() {
+        if (this.element) {
+            typeformEmbed.makeWidget(this.element, "https://adam1238.typeform.com/to/Mw8MZU", {
+                hideHeaders: true,
+                hideFooter: true,
+                opacity: 90
+            });
+        }
+    }
+
     render() {
         return (
             <div className="container-contact">
-                <div className="flex-item-contact">
-                    Contact info: 
-                                Many Hands London
-            
-                                Visit Us!
-                                100 Clements Road
-                                London SE16 4DG
-            
-                                Contact:
-                                info@manyhandslondon.co.uk
-                                0207 101 4669
-            
-                                First Name:
-                                Last Name:
-                                Email Address:
-                                Password:
-            
-                                +++Sign Up+++         ==>JOIN<==
-                                
-                                
-                                Many Hands is part of the Shapes Collective.
-                                www.shapescollective.com
+                <div>
+                    <h2 className="flex-item-contact-title">Contact Us</h2>
+                    <div className="flex-item-contact-form" ref={(element) => this.element = element} />    
+                    <br/>
+                    <p className="flex-item-contact-details">
+                    Our contact details: <br/>
+                    Many Hands London, 100 Clements Road, London SE16 4DG<br/>
+                    Email: info@manyhandslondon.co.uk<br/>
+                    Phone: 0207 101 4669
+                    </p>
                 </div>
             </div>
         );
--- src/FAQ/FAQ.js
+++ src/FAQ/FAQ.js
@@ -5,8 +5,20 @@ export default class FAQ extends React.Component {
     render() {
         return (
             <div className="container-faq">
-                <div className="flex-item-faq">
-                    FAQ
+                <h2 className="flex-item-faq-title">FAQ</h2>
+                <div className="flex-item-faq-text">
+                    <p>
+                        1) Question 1
+                    <br />
+                    </p>
+                    <p>
+                        2) Question 2
+                    <br />
+                    </p>
+                    <p>
+                        3) Question 3
+                    <br />
+                    </p>
                 </div>
             </div>
         );
